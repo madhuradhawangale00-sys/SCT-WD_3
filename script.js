ba8bd6c9748502bfa0ec9e7e1c8beeea8ee7a3ad
madhuradhawangale00-sys <madhuradhawangale00-sys@email.com>
2026-06-13 23:13:13 +0530

    Quize que dataset

---
+++ script.js
@@ -0,0 +1,27 @@
+const quizData = [
+    {
+        question: "Which language runs natively inside a web browser?",
+        options: ["Java", "C", "Python", "JavaScript"],
+        correct: 3
+    },
+    {
+        question: "What does CSS stand for?",
+        options: ["Central Style Sheets", "Cascading Style Sheets", "Cascading System Sheets", "Control Style Sheets"],
+        correct: 1
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopter Text Markup Language"],
+        correct: 0
+    },
+    {
+        question: "Which CSS property controls the text size?",
+        options: ["font-style", "text-style", "font-size", "text-size"],
+        correct: 2
+    },
+    {
+        question: "In what year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "None of the above"],
+        correct: 1
+    }
+];
